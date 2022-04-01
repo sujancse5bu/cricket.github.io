481b14185c2f4ee3bfead191933ea63241bdd0df
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2022-04-01 16:35:59 +0600

    done

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 console.log('Hello Node Js')
+const tempIframe = document.querySelector('#frame iframe')
+const iframeDocument = tempIframe.contentDocument || tempIframe.contentWindow.document;
+if (!iframeDocument) {
+  throw "iframe couldn't be found in DOM.";
+}
+
+var iframeContent = iframeDocument.querySelector('body');
+console.log('body : ', iframeContent)
+iframeContent.addEventListener('click', (e) => {
+  console.log('event: ', e.target)
+  e.preventDefault()
+})
