fa150e0c197ebbd7bc5182fe09faace51d48cf63
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2022-04-03 21:16:13 +0600

    log added

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 const btnsContainer = document.getElementById('btns')
 const contentContainer = document.getElementById('content')
 btnsContainer.addEventListener('click', (event) => {
+  console.log('event.target: ', event.target)
   if (event.target.tagName.toLowerCase() === 'button') {
+    console.log('event.target is button: ', contentContainer.innerHTML)
+    contentContainer.innerHTML = ``
     contentContainer.innerHTML = `<iframe src="https://noob4cast.com/fuckhdstream.php?player=desktop&live=${event.target.id}" width="100%" height="500px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  allow="encrypted-media"></iframe>`
   }
 })
