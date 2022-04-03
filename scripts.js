daff11e0f299bb0e9b71f7cc6c77d625e695e68a
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2022-04-03 20:52:08 +0600

    multiple channel feature added

--- scripts.js
+++ scripts.js
@@ -1,13 +1,7 @@
-console.log('Hello Node Js')
-const tempIframe = document.querySelector('#frame iframe')
-const iframeDocument = tempIframe.contentDocument || tempIframe.contentWindow.document;
-if (!iframeDocument) {
-  throw "iframe couldn't be found in DOM.";
-}
-
-var iframeContent = iframeDocument.querySelector('body');
-console.log('body : ', iframeContent)
-iframeContent.addEventListener('click', (e) => {
-  console.log('event: ', e.target)
-  e.preventDefault()
+const btnsContainer = document.getElementById('btns')
+const contentContainer = document.getElementById('content')
+btnsContainer.addEventListener('click', (event) => {
+  if (event.target.tagName.toLowerCase() === 'button') {
+    contentContainer.innerHTML = `<iframe src="https://noob4cast.com/fuckhdstream.php?player=desktop&live=${event.target.id}" width="100%" height="500px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  allow="encrypted-media"></iframe>`
+  }
 })
